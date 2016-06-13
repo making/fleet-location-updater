28de48fd4977e09155be41f90ea1fff5dbf1c00a
making <makingx@gmail.com>
2016-06-14 02:22:51 +0900

    first commit

---
+++ src/main/java/demo/FleetLocationUpdaterApplication.java
@@ -0,0 +1,53 @@
+package demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import demo.model.CurrentPosition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableBinding(Processor.class)
+@MessageEndpoint
+public class FleetLocationUpdaterApplication {
+
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    ServiceLocationService serviceLocationService;
+
+    @ServiceActivator(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+    public Message<String> out(Message<String> in) throws IOException {
+        CurrentPosition payload = this.objectMapper.readValue(in.getPayload(), CurrentPosition.class);
+        this.serviceLocationService.updateServiceLocations(payload);
+        return MessageBuilder
+                .withPayload(this.objectMapper.writeValueAsString(payload))
+                .copyHeadersIfAbsent(in.getHeaders())
+                .build();
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetLocationUpdaterApplication.class, args);
+    }
+}
---
+++ src/main/java/demo/ServiceLocationService.java
@@ -0,0 +1,73 @@
+package demo;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import demo.model.CurrentPosition;
+import demo.model.ServiceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ServiceLocationService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceLocationService.class);
+
+    @Autowired
+    @LoadBalanced
+    private RestTemplate restTemplate;
+
+    /**
+     * Enriches the provided {@link CurrentPosition} with the closest service location
+     * IF the {@link CurrentPosition} has a {@link demo.model.VehicleStatus} of:
+     *
+     * <ul>
+     *   <li>{@link demo.model.VehicleStatus#SERVICE_NOW}</li>
+     *   <li>{@link demo.model.VehicleStatus#SERVICE_SOON}</li>
+     *   <li>{@link demo.model.VehicleStatus#STOP_NOW}</li>
+     * <ul>
+     *
+     * @param currentPosition Will be enriched with the closest service location
+     * @throws Exception
+     */
+    @HystrixCommand(
+            commandKey="serviceLocations",
+            commandProperties = {
+                    //@HystrixProperty(name = "circuitBreaker.forceOpen", value = "true")
+            },
+            fallbackMethod = "handleServiceLocationServiceFailure")
+    public void updateServiceLocations(CurrentPosition currentPosition) {
+
+        switch (currentPosition.getVehicleStatus()) {
+
+            case SERVICE_NOW:
+            case SERVICE_SOON:
+            case STOP_NOW:
+                ResponseEntity<Resource<ServiceLocation>> result = this.restTemplate.exchange(
+                        "http://service-location-service/serviceLocations/search/findFirstByLocationNear?location={lat},{long}",
+                        HttpMethod.GET, new HttpEntity<>((Void) null),
+                        new ParameterizedTypeReference<Resource<ServiceLocation>>() {
+                        }, currentPosition.getLocation().getLatitude(),
+                        currentPosition.getLocation().getLongitude());
+                if (result.getStatusCode() == HttpStatus.OK
+                        && result.getBody().getContent() != null) {
+                    currentPosition.setServiceLocation(result.getBody().getContent());
+                }
+                break;
+            default:
+        }
+
+    }
+
+    public void handleServiceLocationServiceFailure(CurrentPosition currentPosition) {
+        LOGGER.error("Hystrix Fallback Method. Unable to retrieve service station info.");
+    }
+
+}
